82ab9cdd3481a3e133b1cdcc0d433a3389c6422c
Nikolas Freitas <nikolasbvf@gmail.com>
2019-05-27 00:18:33 -0300

    Add .gitignore

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BORA CODAR SEUS FDP!' });
+  res.status(200).send("Conectou! :D");
 });
 
 module.exports = router;
