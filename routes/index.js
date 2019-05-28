b9d1d06223f7d25ca2d69e4957de33c770584c24
Nikolas Freitas <nikolasbvf@gmail.com>
2019-05-28 02:28:06 -0300

    :sparkles: Add routes to handle more variables and situations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send("Conectou! :D");
+  res.status(200).send("Conectou no meu servidor! :D");
 });
 
 module.exports = router;
--- routes/messages.js
+++ routes/messages.js
@@ -6,9 +6,9 @@ router.post('/', (req, res, next) => {
         const message = req.body.message;
         if (message) {
             res.status(200).send("Recebido com sucesso");
-            console.log(`\n\n Nova mensagem:`);
+            console.log(`\n Nova mensagem:`);
             console.log(message);
-            console.log('=================');
+            console.log('\n =================');
         } else {
             res.status(400).send("<h1>Houve um erro na sua requisição, verifique o seu body</h1>");
         }
@@ -16,5 +16,13 @@ router.post('/', (req, res, next) => {
         res.status(400).send("Houve um erro na sua requisição");
     }
 });
+
+router.get('/', (req,res) => {
+    res.status(400).send("<h1>Somente utilizando post</h1>");
+});
+
+router.get('/segredo', function(req, res, next) {
+    res.status(200).send("PHP não é linguagem de programador");
+  });
 
 module.exports = router;
